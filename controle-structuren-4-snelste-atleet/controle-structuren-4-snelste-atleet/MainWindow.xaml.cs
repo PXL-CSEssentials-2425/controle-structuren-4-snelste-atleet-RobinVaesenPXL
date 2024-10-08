f75e2104ee0fdab0d0d1a2168d47a65221795e73
RobinVaesenPXL <11901804@student.pxl.be>
2024-10-08 17:04:06 +0200

    MOVIE-01 Changed background color of window to PXL Green

--- controle-structuren-4-snelste-atleet/controle-structuren-4-snelste-atleet/MainWindow.xaml.cs
+++ controle-structuren-4-snelste-atleet/controle-structuren-4-snelste-atleet/MainWindow.xaml.cs
@@ -16,9 +16,51 @@ namespace controle_structuren_4_snelste_atleet
     /// </summary>
     public partial class MainWindow : Window
     {
+        string fastestAthlete;
+        int fastestTime = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void newButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool isValidNumber = int.TryParse(timeTextBox.Text, out int inputTime);
+
+            if (fastestTime == 0 || inputTime < fastestTime)
+            {
+                fastestTime = inputTime;
+                fastestAthlete = nameTextBox.Text;
+            }
+
+            nameTextBox.Clear();
+            timeTextBox.Clear();
+        }
+
+        private void fastestButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultTextBox.Text = $"De snelste atleet is {fastestAthlete}\n" +
+                $"totaal aantal seconden: {fastestTime}\n\n" +
+                $"aantal uren: {fastestTime / 24}" +
+                $"aantal minuten: {fastestTime / 24 / 60}" +
+                $"aantal seconden: {fastestTime / 24 / 60 / 60}";
+
+            nameTextBox.Focus();
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            nameTextBox.Clear();
+            timeTextBox.Clear();
+            resultTextBox.Clear();
+
+            nameTextBox.Focus();
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
